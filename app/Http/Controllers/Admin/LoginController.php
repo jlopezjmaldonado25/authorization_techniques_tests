705dc48cfa319bfa71c5de280fb676a226e33423
Julio <jlopez25.cursos@gmail.com>
2020-08-20 18:01:38 +0000

    Cierre de sesión con autenticación múltiple en Laravel (con TDD)

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
@@ -58,4 +59,30 @@ class LoginController extends Controller
     {
         return Auth::guard('admin');
     }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        if (Auth::guard('web')->guest()) {
+            $request->session()->invalidate();
+        }
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new Response('', 204)
+            : redirect('/');
+    }
+
 }
---
+++ tests/Feature/Admin/AdminLogoutTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AdminLogoutTest extends TestCase
+{
+    /** @test */
+    function an_admin_can_logout()
+    {
+        // Having / Given / Arrange
+        auth('admin')->login($this->createAdmin());
+
+        $this->assertAuthenticated('admin');
+
+        // When / Act
+        $response = $this->post('admin/logout');
+
+        // Then / Assert
+        $response->assertRedirect('/');
+
+        $this->assertGuest('admin');
+    }
+
+    /** @test */
+    function logging_out_as_an_admin_does_not_terminate_the_user_session()
+    {
+        // Having / Given / Arrange
+        auth('admin')->login($this->createAdmin());
+        auth('web')->login($this->createUser());
+
+        $adminSessionName = auth('admin')->getName();
+        $webSessionName = auth('web')->getName();
+
+        //dd(Session::all());
+
+        $this->assertAuthenticated('admin');
+        $this->assertAuthenticated('web');
+
+        // When / Act
+        $response = $this->post('admin/logout');
+
+        // Then / Assert
+        $response->assertRedirect('/')
+            ->assertSessionHas($webSessionName)
+            ->assertSessionMissing($adminSessionName);
+
+        $this->assertGuest('admin');
+        $this->assertAuthenticated('web');
+    }
+}
